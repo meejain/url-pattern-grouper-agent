0db031273181c18962cb3abe188a290e34247414
Meet Kumar Jain <meetkjain83@gmail.com>
2025-07-07 16:24:46 +0530

    Fixing the lint issues

--- github_utils.py
+++ github_utils.py
@@ -1,11 +1,12 @@
-
 from github import Github
+from github.ContentFile import ContentFile
 import os
 
 def download_json_file(token, repo_name, file_path):
     g = Github(token)
     repo = g.get_repo(repo_name)
-    file_content = repo.get_contents(file_path)
+    contents = repo.get_contents(file_path)
+    file_content = contents if isinstance(contents, ContentFile) else contents[0]
     with open("site-urls.json", "w") as f:
         f.write(file_content.decoded_content.decode())
     return "site-urls.json"
@@ -17,6 +18,7 @@ def upload_excel_file(token, repo_name, file_path, commit_message):
         content = f.read()
     try:
         contents = repo.get_contents(file_path)
-        repo.update_file(contents.path, commit_message, content, contents.sha)
+        file_content = contents if isinstance(contents, ContentFile) else contents[0]
+        repo.update_file(file_content.path, commit_message, content, file_content.sha)
     except:
         repo.create_file(file_path, commit_message, content)
